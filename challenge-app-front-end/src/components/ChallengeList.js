fe0efeab589bb3ec2805e1472835a4a19aa07ad2
JohnUfo <tursunov.umidjon.uz@gmail.com>
2025-03-08 22:14:26 +0500

    everythin working before aws check

--- challenge-app-front-end/src/components/ChallengeList.js
+++ challenge-app-front-end/src/components/ChallengeList.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
 import Challenge from "./Challenge";
-import axios from "axios";
 import EditChallenge from "./EditChallenge";
-import "./ChallengeList.css";
+import axios from "axios";
+import {useState} from "react";
 
 function ChallengeList({ challenges, onChallengeUpdated, onChallengeDeleted }) {
-  const [editingChallenge, setEditingChallenge] = useState(null); 
+  const safeChallenges = Array.isArray(challenges) ? challenges : [];
+  const [editingChallenge, setEditingChallenge] = useState(null);
 
   const handleDelete = async (id) => {
     try {
@@ -20,33 +20,33 @@ function ChallengeList({ challenges, onChallengeUpdated, onChallengeDeleted }) {
     try {
       await axios.put(`http://localhost:8080/challenges/${updatedChallenge.id}`, updatedChallenge);
       setEditingChallenge(null);
-      onChallengeUpdated(); 
+      onChallengeUpdated(); // Refresh the list of challenges
     } catch (error) {
       console.error("Error updating challenge:", error);
     }
   };
 
   return (
-    <div className="challenge-list">
-      <h2>Challenges</h2>
-      {challenges.map((challenge) => (
-        <div key={challenge.id}>
-          {editingChallenge === challenge.id ? (
-            <EditChallenge
-              challenge={challenge}
-              onUpdate={handleUpdate}
-              onCancel={() => setEditingChallenge(null)}
-            />
-          ) : (
-            <Challenge
-              challenge={challenge}
-              onEdit={() => setEditingChallenge(challenge.id)}
-              onDelete={() => handleDelete(challenge.id)}
-            />
-          )}
-        </div>
-      ))}
-    </div>
+      <div className="challenge-list">
+        <h2>Challenges</h2>
+        {safeChallenges.map((challenge) => (
+            <div key={challenge.id}>
+              {editingChallenge === challenge.id ? (
+                  <EditChallenge
+                      challenge={challenge}
+                      onUpdate={handleUpdate}
+                      onCancel={() => setEditingChallenge(null)}
+                  />
+              ) : (
+                  <Challenge
+                      challenge={challenge}
+                      onEdit={() => setEditingChallenge(challenge.id)}
+                      onDelete={() => handleDelete(challenge.id)}
+                  />
+              )}
+            </div>
+        ))}
+      </div>
   );
 }
 
